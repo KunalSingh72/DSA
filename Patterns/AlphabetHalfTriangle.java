e2159b15604a6b4f59e483bc8cee7f4fd4650d5e
Draco-Byte <mrk33256@gmail.com>
2024-02-21 20:11:13 +0530

    .

---
+++ Patterns/AlphabetHalfTriangle.java
@@ -0,0 +1,31 @@
+package Patterns;
+import java.util.Scanner;
+
+public class AlphabetHalfTriangle {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        int row = s.nextInt();
+        
+        for (int i = 0; i < row; i++) {
+            int alpha = 65;
+            for (int j = 0; j <=i; j++) {
+                System.out.print(((char)alpha));
+                alpha++;
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        for (int i = row; i >0; i--) {
+            char alpha = 65;
+
+            for (int j = 1; j <=i; j++) {
+                System.out.print(alpha);
+                alpha++;
+            }
+            System.out.println();
+        }
+        s.close();
+    }
+}
